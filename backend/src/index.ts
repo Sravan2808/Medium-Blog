12520dcaf4fcae4c61488e334a02d4b75361259d
Sravan Surya <vsravansurya@gmail.com>
2025-06-10 20:08:03 +0530

    Add JWT to signup route and Add a Signin route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,34 +1,62 @@
 import { Hono } from "hono";
-import { PrismaClient } from '../app/generated/prisma/client/edge'
-import { withAccelerate } from '@prisma/extension-accelerate'
-const prisma = new PrismaClient()
+import { PrismaClient } from "../app/generated/prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { sign } from "hono/jwt";
+const prisma = new PrismaClient();
 const app = new Hono<{
   Bindings: {
     DATABASE_URL: string;
+    JWT_SCERET: string;
   };
 }>();
 
-app.post('/api/v1/signup', async (c) => {
-	const prisma = new PrismaClient({
-		datasourceUrl: c.env?.DATABASE_URL,
-	}).$extends(withAccelerate());
-	const body = await c.req.json();
-	try {
-		const user = await prisma.user.create({
-			data: {
-				email: body.email,
-				password: body.password
-			}
-		});
-	
-		return c.text('jwt here')
-	} catch(e) {
-		return c.status(403);
-	}
-})
+app.post("/api/v1/signup", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env?.DATABASE_URL,
+  }).$extends(withAccelerate());
 
-app.post("/api/v1/signin", (c) => {
-  return c.text("signin route");
+  const body = await c.req.json();
+  try {
+    const user = await prisma.user.create({
+      data: {
+        email: body.email,
+        password: body.password,
+      },
+    });
+    const jwt = await sign({ id: user.id }, c.env.JWT_SCERET);
+    return c.json({ jwt });
+  } catch (e) {
+    console.log(e);
+
+    c.status(403);
+    return c.json({ error: "error while signing up" });
+  }
+});
+
+app.post("/api/v1/signin", async(c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env?.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json();
+  try {
+    const user = await prisma.user.findFirst({
+      where: {
+        email: body.email,
+        password: body.password,
+      },
+    });
+    if (!user) {
+      c.status(403);
+      return c.json({ error: "user not found" });
+    }
+    const jwt = await sign({ id: user.id }, c.env.JWT_SCERET);
+    return c.json({ jwt });
+  } catch (e) {
+    console.log(e);
+    c.status(403);
+    return c.json({ error: "error while signing in" });
+  }
 });
 
 app.get("/api/v1/blog/:id", (c) => {
